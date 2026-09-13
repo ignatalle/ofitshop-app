177d24366bb7cc9b8b52598d9efe61cb4caa976d
ignatalle <ignatallle@gmail.com>
2026-09-13 19:24:12 -0300

    fix: wrap clientes route for mobile layout

---
+++ src/app/clientes/layout.tsx
@@ -0,0 +1,5 @@
+import './clientes-mobile.css';
+
+export default function ClientesLayout({ children }: { children: React.ReactNode }) {
+  return <div className="clientes-mobile">{children}</div>;
+}
