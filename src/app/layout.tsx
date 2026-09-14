0518c238e910dc5d2ac00ddda8c6395ddea3726e
ignatalle <ignatallle@gmail.com>
2026-09-13 21:21:03 -0300

    style: activate full-app luxe feminine polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./mobile-audit.css";
 import "./luxe-theme.css";
+import "./luxe-all.css";
 import Sidebar from "../components/Sidebar";
 import BottomNav from "../components/BottomNav";
 
