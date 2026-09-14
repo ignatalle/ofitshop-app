e984b2507af3e0f92e47a5466c30f1426937a54c
ignatalle <ignatallle@gmail.com>
2026-09-13 21:16:55 -0300

    style(clientes): load premium dark section styles

--- src/app/clientes/layout.tsx
+++ src/app/clientes/layout.tsx
@@ -1,4 +1,5 @@
 import './clientes-mobile.css';
+import './princess.css';
 import ClientesOrganizer from './ClientesOrganizer';
 
 export default function ClientesLayout({ children }: { children: React.ReactNode }) {
