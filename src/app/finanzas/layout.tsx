d86f957990dcc3f4b54bec86f1f5903a8cafb165
ignatalle <ignatallle@gmail.com>
2026-09-13 21:12:59 -0300

    style: load princess premium Finanzas theme

--- src/app/finanzas/layout.tsx
+++ src/app/finanzas/layout.tsx
@@ -1,4 +1,5 @@
 import './mobile.css';
+import './princess.css';
 
 export default function FinanzasLayout({ children }: { children: React.ReactNode }) {
   return <div className="finanzas-mobile-shell">{children}</div>;
